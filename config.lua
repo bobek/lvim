eed9a842ffcf35b9151cd71b15f4cffffe25e791
Antonin Kral <a.kral@bobek.cz>
2022-07-22 19:25:07 +0200

    Make buffer switcher smaller and allow deletion of buffers

--- config.lua
+++ config.lua
@@ -20,21 +20,25 @@ lvim.keys.insert_mode["<C-b>"] = "<cmd>Telescope buffers<cr>"
 
 lvim.builtin.telescope.pickers.buffers = {
   ignore_current_buffer = true,
+  previewer = false,
   sort_mru = true,
+  theme = "dropdown",
 }
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+lvim.builtin.telescope.on_config_done = function()
+  local actions = require "telescope.actions"
+  lvim.builtin.telescope.defaults.mappings.i["<c-d>"] = actions.delete_buffer
+
+  --   -- for input mode
+  --   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+  --   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+  --   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+  --   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+  --   -- for normal mode
+  --   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+  --   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+end
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
