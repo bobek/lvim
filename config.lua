457d8becb48c35cfae4f4f9ca4c117750084e8ee
Antonin Kral <a.kral@bobek.cz>
2022-09-27 12:58:01 +0200

    nvimtree

--- config.lua
+++ config.lua
@@ -69,7 +69,8 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.view.side = "left"
+-- lvim.builtin.nvimtree.show_icons.git = 0
 
 lvim.builtin.notify.active = true
 lvim.builtin.breadcrumbs.active = true
@@ -80,9 +81,6 @@ lvim.builtin.lualine.sections.lualine_y = {
 }
 
 lvim.builtin.dap.active = true
-
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
