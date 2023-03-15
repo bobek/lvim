1a7fdb8722065714766f3db97962a5ab64b9d127
Antonin Kral <a.kral@bobek.cz>
2023-03-15 14:16:35 +0100

    TabNine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,6 +75,19 @@ lvim.plugins = {
     }
   },
 
+  {
+    'codota/tabnine-nvim',
+    build = "./dl_binaries.sh",
+    config = function()
+      require('tabnine').setup({
+        disable_auto_comment = true,
+        accept_keymap = "<Tab>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" }
+      })
+    end
   },
 
   {
